b4cd1383c77a39d33892c0088746c045322e3da1
geodimitrov <geo.dimitrov@yahoo.com>
2021-07-31 22:04:43 +0300

    Made changes to main app

--- book_center/book_center/bc_main/urls.py
+++ book_center/book_center/bc_main/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from book_center.bc_main.views import contact_view, HomeView, AboutView
+from book_center.bc_main.views import HomeView, AboutView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('about/', AboutView.as_view(), name='about'),
-    path('contact/', contact_view, name='contact'),
 ]
--- book_center/book_center/bc_main/views.py
+++ book_center/book_center/bc_main/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render, redirect
-from django.views.generic import TemplateView, ListView
-from book_center.bc_main.forms import ContactForm
+from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
@@ -9,16 +7,3 @@ class HomeView(TemplateView):
 
 class AboutView(TemplateView):
     template_name = 'main/about.html'
-
-
-def contact_view(request):
-    if request.method == "POST":
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            return redirect('home')
-
-    form = ContactForm()
-    context = {
-        'form': form,
-    }
-    return render(request, 'main/contact.html', context)
