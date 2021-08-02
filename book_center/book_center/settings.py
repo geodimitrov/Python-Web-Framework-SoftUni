bd796f24d08136c976ca7d953c9006c08d49df74
geodimitrov <geo.dimitrov@yahoo.com>
2021-08-02 12:17:26 +0300

    Made changes to settings

--- book_center/book_center/settings.py
+++ book_center/book_center/settings.py
@@ -1,18 +1,16 @@
 import os
-
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import django.core.mail.backends.console
 from django.urls import reverse_lazy
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&$5je8bb6qy%ds^zxa$t^vn7uwyc+ic_od9qpyuf%ubbpcdlv^'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,6 +30,8 @@ INSTALLED_APPS = [
     'book_center.bc_main',
     'book_center.bc_events',
     'book_center.bc_profiles',
+    'book_center.bc_contact',
+    'book_center.bc_blog',
 ]
 
 MIDDLEWARE = [
@@ -65,9 +65,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'book_center.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# WARNING!!! USE POSTGRESQL, hide credentials
 
 DATABASES = {
     'default': {
@@ -75,7 +75,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -105,6 +104,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+EMAIL_BACKEND = 'django_ses.SESBackend'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_SES_REGION_NAME = os.environ.get('AWS_SES_REGION_NAME')
+AWS_SES_REGION_ENDPOINT = os.environ.get('AWS_SES_REGION_ENDPOINT')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
