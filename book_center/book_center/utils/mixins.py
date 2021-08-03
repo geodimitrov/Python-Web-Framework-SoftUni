5086ad8858e82e459744586c8089d1eec5b59f83
geodimitrov <geo.dimitrov@yahoo.com>
2021-08-03 12:57:47 +0300

    Added mixin for autocomplete off

--- book_center/book_center/utils/mixins.py
+++ book_center/book_center/utils/mixins.py
@@ -3,3 +3,10 @@ class NoLabelFormMixin:
         for (_, field) in self.fields.items():
             field.label = ''
 
+
+class DisableAutocompleteMixin:
+    def _init_bootstrap(self):
+        for (_, field) in self.fields.items():
+            field.widget.attrs['autocomplete'] = 'off'
+
+
--- book_center/book_center/utils/utils.py
+++ book_center/book_center/utils/utils.py
@@ -3,16 +3,14 @@ from django.template.loader import render_to_string
 
 
 def send_confirmation_email(recipient_email):
-    pass
-    # subject = 'Message Received'
-    # message = 'Thank you for contacting us. We have received your message. '
-    # sender_email = 'book_center_notifications@protonmail.com'
-    # return send_mail(subject, message, sender_email, [recipient_email])
+    subject = 'Message Received'
+    message = 'Thank you for contacting us. We have received your message.'
+    sender_email = 'book_center_notifications@protonmail.com'
+    return send_mail(subject, message, sender_email, [recipient_email])
 
 
 def send_verification_email(recipient_email, context):
-    pass
-    # subject = 'Verify Your Email Address'
-    # message = render_to_string('auth/verification/verify_email_msg.html', context)
-    # sender_email = 'book_center_notifications@protonmail.com'
-    # return send_mail(subject, message, sender_email, [recipient_email])
+    subject = 'Verify Your Email Address'
+    message = render_to_string('auth/email_verification/verify_email_msg.html', context)
+    sender_email = 'book_center_notifications@protonmail.com'
+    return send_mail(subject, message, sender_email, [recipient_email])
